3642f8a46bb307ae1c190a503a255449e9fc2249
zyegfryed <zyegfryed@gmail.com>
2012-04-02 17:04:41 +0200

    Initial commit.

---
+++ README.rst
@@ -0,0 +1,59 @@
+=================
+django-statici18n
+=================
+
+This is a Django app that provides helper for generating Javascript catalog
+to static files.
+
+When dealing with internationalization in Javascript code, Django provides the
+`javascript_catalog view`_ which sends out a JavaScript code library with
+functions that mimic the gettext interface, plus an array of translation
+strings.
+
+At first glance, it works well and everything is fine. However, for a given
+language, each request will generates the same identical catalog.
+
+That's what ``statici18n`` is for:
+
+    Collecting Javascript catalogs from each of your Django apps (and any other
+    place you specify) into a single location that can easily be served in
+    production.
+
+The main website for django-staticfiles is
+`github.com/zyegfryed/django-statici18n`_ where you can also file tickets.
+
+.. javascript_catalog view: http://docs.djangoproject.com/en/1.4/topics/i18n/translation/#module-django.views.i18n
+
+Installation
+------------
+
+- Use your favorite Python packaging tool to install ``statici18n``
+  from `PyPI`_, e.g.::
+
+    pip install django-statici18n
+
+- Added ``'statici18n'`` to your ``INSTALLED_APPS`` setting::
+
+    INSTALLED_APPS = [
+        # ...
+        'statici18n',
+    ]
+
+- Once you have translated and compiled your messages, use the ``collecti18n``
+  management command::
+
+    python manage.py collecti18n
+
+By default, the generated catalogs are stored to ``STATIC_ROOT``.
+
+- Edit your templates and replace the dynamically generated script by the
+  statically generated script like this::
+
+    <script src="{{ STATIC_URL }}jsi18n/{{ LANGUAGE_CODE }}/djangojs.js"></script>
+
+.. _github.com/zyegfryed/django-statici18n: http://github.com/zyegfryed/django-statici18n
+.. _PyPI: http://pypi.python.org/pypi/django-statici18n
+.. _Apache: http://httpd.apache.org/
+.. _Lighttpd: http://www.lighttpd.net/
+.. _Nginx: http://wiki.nginx.org/
+.. _Cherokee: http://www.cherokee-project.com/
---
+++ statici18n/management/commands/collecti18n.py
@@ -0,0 +1,162 @@
+from __future__ import with_statement
+
+import os
+import gettext as gettext_module
+from optparse import make_option
+
+from django.core.management.base import NoArgsCommand
+from django.conf import settings
+from django.utils import importlib
+from django.utils.translation import to_locale
+from django.utils.text import javascript_quote
+from django.views.i18n import (LibHead, LibFoot, LibFormatHead, LibFormatFoot,
+                               SimplePlural, InterPolate, PluralIdx, get_formats)
+
+
+# This function is a ripoff of `django.views.i18n.javascript_catalog with
+# all the request specific code removed.
+def javascript_catalog(locale, domain, packages=None):
+    if packages is None:
+        packages = ['django.conf']
+    if isinstance(packages, basestring):
+        packages = packages.split('+')
+    packages = [p for p in packages if p == 'django.conf' or p in settings.INSTALLED_APPS]
+    default_locale = to_locale(settings.LANGUAGE_CODE)
+    t = {}
+    paths = []
+    en_selected = locale.startswith('en')
+    en_catalog_missing = True
+    # paths of requested packages
+    for package in packages:
+        p = importlib.import_module(package)
+        path = os.path.join(os.path.dirname(p.__file__), 'locale')
+        paths.append(path)
+    # add the filesystem paths listed in the LOCALE_PATHS setting
+    paths.extend(list(reversed(settings.LOCALE_PATHS)))
+    # first load all english languages files for defaults
+    for path in paths:
+        try:
+            catalog = gettext_module.translation(domain, path, ['en'])
+            t.update(catalog._catalog)
+        except IOError:
+            pass
+        else:
+            # 'en' is the selected language and at least one of the packages
+            # listed in `packages` has an 'en' catalog
+            if en_selected:
+                en_catalog_missing = False
+    # next load the settings.LANGUAGE_CODE translations if it isn't english
+    if default_locale != 'en':
+        for path in paths:
+            try:
+                catalog = gettext_module.translation(domain, path, [default_locale])
+            except IOError:
+                catalog = None
+            if catalog is not None:
+                t.update(catalog._catalog)
+    # last load the currently selected language, if it isn't identical to the default.
+    if locale != default_locale:
+        # If the currently selected language is English but it doesn't have a
+        # translation catalog (presumably due to being the language translated
+        # from) then a wrong language catalog might have been loaded in the
+        # previous step. It needs to be discarded.
+        if en_selected and en_catalog_missing:
+            t = {}
+        else:
+            locale_t = {}
+            for path in paths:
+                try:
+                    catalog = gettext_module.translation(domain, path, [locale])
+                except IOError:
+                    catalog = None
+                if catalog is not None:
+                    locale_t.update(catalog._catalog)
+            if locale_t:
+                t = locale_t
+    src = [LibHead]
+    plural = None
+    if '' in t:
+        for l in t[''].split('\n'):
+            if l.startswith('Plural-Forms:'):
+                plural = l.split(':', 1)[1].strip()
+    if plural is not None:
+        # this should actually be a compiled function of a typical plural-form:
+        # Plural-Forms: nplurals=3; plural=n%10==1 && n%100!=11 ? 0 : n%10>=2 && n%10<=4 && (n%100<10 || n%100>=20) ? 1 : 2;
+        plural = [el.strip() for el in plural.split(';') if el.strip().startswith('plural=')][0].split('=', 1)[1]
+        src.append(PluralIdx % plural)
+    else:
+        src.append(SimplePlural)
+    csrc = []
+    pdict = {}
+    for k, v in t.items():
+        if k == '':
+            continue
+        if isinstance(k, basestring):
+            csrc.append("catalog['%s'] = '%s';\n" % (javascript_quote(k), javascript_quote(v)))
+        elif isinstance(k, tuple):
+            if k[0] not in pdict:
+                pdict[k[0]] = k[1]
+            else:
+                pdict[k[0]] = max(k[1], pdict[k[0]])
+            csrc.append("catalog['%s'][%d] = '%s';\n" % (javascript_quote(k[0]), k[1], javascript_quote(v)))
+        else:
+            raise TypeError(k)
+    csrc.sort()
+    for k, v in pdict.items():
+        src.append("catalog['%s'] = [%s];\n" % (javascript_quote(k), ','.join(["''"] * (v + 1))))
+    src.extend(csrc)
+    src.append(LibFoot)
+    src.append(InterPolate)
+    src.append(LibFormatHead)
+    src.append(get_formats())
+    src.append(LibFormatFoot)
+    src = ''.join(src)
+    return src
+
+
+class Command(NoArgsCommand):
+    option_list = NoArgsCommand.option_list + (
+        make_option('--locale', '-l', dest='locale',
+            help='The locale to process. Default is to process all.'
+        ),
+        make_option('-d', '--domain',
+            dest='domain', default='djangojs',
+            help="Override the gettext domain. By default, the command uses "
+            "the djangojs gettext domain."),
+        make_option('-p', '--packages', action='append', default=[],
+            dest='packages', metavar='PACKAGE',
+            help="A list of packages to check for translations. Default is "
+            "'django.conf'."),
+        make_option('-o', '--output', dest='outputdir', metavar='OUTPUT_DIR',
+            help="Output directory to store generated catalogs. Defaults to "
+            "<STATIC_ROOT>/jsi18n.")
+    )
+    help = "Collect Javascript catalog files in a single location."
+
+    def handle_noargs(self, **options):
+        locale = options.get('locale')
+        domain = options['domain']
+        packages = options['packages'] or None
+        outputdir = options['outputdir']
+        verbosity = int(options.get('verbosity'))
+
+        if locale is not None:
+            languages = [locale]
+        else:
+            languages = [to_locale(lang_code) for (lang_code, lang_name) in settings.LANGUAGES]
+
+        if outputdir is None:
+            outputdir = os.path.join(settings.STATIC_ROOT, 'jsi18n')
+
+        for locale in languages:
+            if verbosity > 0:
+                print "processing language", locale
+
+            basedir = os.path.join(outputdir, locale)
+            if not os.path.isdir(basedir):
+                os.makedirs(basedir)
+
+            src = javascript_catalog(locale, domain, packages)
+            jsfile = os.path.join(basedir, '%s.js' % domain)
+            with open(jsfile, 'w') as f:
+                f.write(src)
