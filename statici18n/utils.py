b780a26da47703c066cfef269c8d65be6b9c4e4c
Marc K <trojjer@gmail.com>
2013-06-13 10:05:20 +0200

    Corrected default_filename with to_locale(get_language())
    There was an ImportError.

--- statici18n/utils.py
+++ statici18n/utils.py
@@ -33,5 +33,5 @@ def get_filename(*args, **kwargs):
 
 
 def default_filename(locale, domain):
-    from django.utils.translation import to_language
-    return os.path.join(to_language(locale), '%s.js' % domain)
+    from django.utils.translation import to_locale, get_language
+    return os.path.join(to_locale(get_language()), '%s.js' % domain)
