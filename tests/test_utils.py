9c5a72ea390014e01b4fa9f143a98d41df8d4ac2
zyegfryed <zyegfryedgmail.com>
2013-11-14 22:12:11 +0100

    Refactored utils tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -3,41 +3,32 @@ import pytest
 from statici18n import utils
 
 
-###############################################################################
-
 def test_default_filename_with_language():
-    filename = utils.default_filename('en', 'djangojs')
+    filename = utils.get_filename('en', 'djangojs')
     assert filename == 'en/djangojs.js'
 
-    filename = utils.default_filename('en-us', 'djangojs')
+    filename = utils.get_filename('en-us', 'djangojs')
     assert filename == 'en-us/djangojs.js'
 
 
 def test_default_filename_with_locale():
-    filename = utils.default_filename('en_GB', 'djangojs')
+    filename = utils.get_filename('en_GB', 'djangojs')
     assert filename == 'en-gb/djangojs.js'
 
 
-###############################################################################
-
-def test_get_filename_default():
-    filename = utils.get_filename('fr-CH', 'djangojs')
-    assert filename == 'fr-ch/djangojs.js'
-
-
-def js_filename(locale, domain):
+def custom_func(locale, domain):
     return "{0}-{1}.js".format(locale, domain)
 
 
-def test_get_filename_custom(settings):
-    settings.STATICI18N_FILENAME_FUNCTION = 'test_utils.js_filename'
+def test_filename_with_custom_func(settings):
+    settings.STATICI18N_FILENAME_FUNCTION = 'test_utils.custom_func'
 
     filename = utils.get_filename('es', 'djangojs')
     assert filename == 'es-djangojs.js'
 
 
-def test_get_filename_not_exists(settings):
-    settings.STATICI18N_FILENAME_FUNCTION = 'my_filename'
+def test_filename_with_no_func(settings):
+    settings.STATICI18N_FILENAME_FUNCTION = 'no_func'
 
     with pytest.raises(ImportError):
         utils.get_filename('es', 'djangojs')
