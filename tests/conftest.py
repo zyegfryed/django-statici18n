881054b8eceddc6a0773ee7e3514b9b9d6b20f4d
Sébastien Fievet <_@sebastien-fievet.fr>
2015-03-18 11:37:08 +0100

    Update dependencies, add Python 3.2 and Django 1.7 test support

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,13 +1,15 @@
-import os
-import sys
+# import os
+# import sys
 import shutil
 import pytest
 
 
-def pytest_configure():
-    BASE_DIR = os.path.join(os.path.dirname(__file__))
-    sys.path.append(os.path.realpath(os.path.join(BASE_DIR, "test_project")))
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
+# FIXME: wait for #216 to be merged
+# See: https://github.com/pytest-dev/pytest-django/pull/216
+# def pytest_configure():
+#    BASE_DIR = os.path.join(os.path.dirname(__file__))
+#    sys.path.append(os.path.realpath(os.path.join(BASE_DIR, "test_project")))
+#    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
 
 
 @pytest.fixture
