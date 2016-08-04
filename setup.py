7c88e4e0f265a74a9a33d7351c99df96cd2d39d2
Julen Ruiz Aizpuru <julenx@gmail.com>
2016-08-04 11:28:51 +0200

    setup.py: bump Django version in `install_requires`

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     include_package_data=True,
     zip_safe=False,
     install_requires=[
-        "Django>=1.4",
+        "Django>=1.8",
         "django-appconf>=0.6",
     ],
     license="BSD",
