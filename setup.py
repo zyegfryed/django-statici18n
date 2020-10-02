5fdf378f4efd51774004e57dbbb563cb3907c82d
Sébastien Fievet <_@sebastien-fievet.fr>
2020-10-02 15:27:33 +0200

    Add project URL

--- setup.py
+++ setup.py
@@ -37,4 +37,7 @@ setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
     ],
+    project_urls={
+        'Source': 'https://github.com/zyegfryed/django-statici18n',
+    }
 )
