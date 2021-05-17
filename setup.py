cbe77296efe068e7efb388a9f52f320e67bf4720
Sébastien Fievet <sebastien.fievet@exoscale.ch>
2021-05-17 15:51:11 +0200

    Add Django 3.2 support / Remove Django 3.0 support

--- setup.py
+++ setup.py
@@ -31,6 +31,7 @@ setup(
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
     project_urls={
         "Source": "https://github.com/zyegfryed/django-statici18n",
