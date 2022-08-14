f011a99f93b70a99184eacf77fe7971038583394
Sébastien Fievet <_@sebastien-fievet.net>
2022-08-14 21:36:02 +0200

    Reflect supported Python versions in classifiers

--- setup.py
+++ setup.py
@@ -27,12 +27,11 @@ setup(
         "Operating System :: OS Independent",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.4",
-        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
     ],
     project_urls={
         "Source": "https://github.com/zyegfryed/django-statici18n",
