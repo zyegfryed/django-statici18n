b1eb54cac2d4fa4da1ceea640b4f0e14aebc557d
irtazaakram <irtaza.akram@arbisoft.com>
2023-07-26 15:00:43 +0500

    feat: add 3.11 support - added back python 3.6 & 3.7 support

--- setup.py
+++ setup.py
@@ -27,9 +27,12 @@ setup(
         "Operating System :: OS Independent",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
     ],
     project_urls={
         "Source": "https://github.com/zyegfryed/django-statici18n",
